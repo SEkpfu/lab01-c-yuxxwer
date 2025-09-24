64d42654872d2b31768bbb59aef012b0446b2bae
yuxxwer <russodara411@gmail.com>
2025-09-24 18:21:31 +0500

    Add files via upload

---
+++ z1.c
@@ -0,0 +1,11 @@
+#include ‹stdio.h›
+#include ‹math.h>
+int main(){
+double a,b,c;
+printf("a= ");
+scanf("%1f", &a);
+double volume=a*a*a;
+double area=6*a*a;
+printf("volume=%f, area=%f" , volume, area);
+return 0;
+}
